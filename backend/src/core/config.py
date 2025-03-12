3610f9c8328885d6bea0efc247e6ae2f9c76fdd3
Timofey <Timofeygorev.steam@gmail.com>
2025-03-12 21:53:14 +0500

    backend db core update

---
+++ backend/src/core/config.py
@@ -0,0 +1,62 @@
+from pydantic_settings import BaseSettings
+from time import time
+from loguru import logger
+from datetime import datetime, timedelta
+
+class DatabaseSettings(BaseSettings):
+    DATABASE_URL: str
+    POOL_SIZE: int = 50
+    MAX_OVERFLOW: int = 10
+
+class RoutersPrefix(BaseSettings):
+    pass
+
+class LoggerSettings(BaseSettings):
+    filename: str = "app"
+    extension: str = ".log"
+    level: str = "DEBUG"
+    rotation: str = "12:00"
+    serialize: bool = False
+    compression: str = "zip"
+    format: str = "{time} {level} {message}"
+
+class Settings(BaseSettings):
+    SERVER_START_TIME: int
+    SERVER_PORT: int = 8765
+    IP_ADDRESS: str = 'localhost'
+    db: DatabaseSettings
+    prefix: RoutersPrefix
+    logger: LoggerSettings
+
+
+settings = Settings(
+    SERVER_START_TIME=int(time()),
+    SERVER_PORT=8765,
+    IP_ADDRESS='127.0.0.1',
+    db=DatabaseSettings(
+        DATABASE_URL='postgresql+asyncpg://postgres:postgres@localhost:5432/postgres',
+        POOL_SIZE=50,
+        MAX_OVERFLOW=10
+    ),
+    prefix=RoutersPrefix(
+    ),
+    logger=LoggerSettings(
+        filename="app",
+        extension=".log",
+        level="DEBUG",
+        compression="zip",
+        rotation="5MB",
+        serialize=False,
+        format="{time} {level} {message}"
+    )
+)
+
+
+logger.add(
+    sink=f'{settings.logger.filename}{settings.logger.extension}',
+    level=settings.logger.level,
+    format=settings.logger.format,
+    serialize=settings.logger.serialize,
+    rotation=settings.logger.rotation,
+    compression=settings.logger.compression,
+)
