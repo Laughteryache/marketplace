362f07d15ea83df8e4b4c44d409ca3c7ddf86d04
Laughteryache <laughteryache@gmail.com>
2025-03-15 22:00:05 +0500

    added header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import HeaderTop from "./HeaderTop";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <HeaderTop className="header_top"></HeaderTop>
+    </header>
+  );
+};
+
+export default Header;
