1c96f0456e15d6761f3842657e1c2843051a9088
Timofey <Timofeygorev.steam@gmail.com>
2025-03-15 23:48:17 +0500

    Add balance retrieval and user interface endpoint
    Implement user and business balance retrieval logic in database functions and expose these through a new `/balance` endpoint in the user interface router. Update associated logic for handling token payloads and include necessary schemas.

--- backend/src/routers/profile/user_interface.py
+++ backend/src/routers/profile/user_interface.py
@@ -1,10 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Response, Cookie
+from fastapi import APIRouter, Depends, HTTPException, status, Cookie
 from fastapi.responses import JSONResponse
+
+from pydantic import BaseModel
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.config import settings
-from services.security import JWTAuth
+from core.schemes import TokenPayloadModel
+
+from services.security import JWTAuth, get_payload_by_access_token
+
+from core.database.functions import UsersDB, BusinessDB
+from core.database.helper import db_helper
 
 
 router = APIRouter(
@@ -12,13 +19,24 @@ router = APIRouter(
     tags=["User Interface"]
 )
 
+class BalanceInfo(BaseModel):
+    balance: int
 
 
 @router.get('/balance')
 async def get_user_balance(
-        response: Response,
-        access_token: ...,
-        refresh_token: ...,
+        token_payload: TokenPayloadModel = Depends(get_payload_by_access_token),
         session: AsyncSession = Depends(db_helper.get_async_session)
 ) -> JSONResponse:
-    pass
+    if token_payload.role == 'user':
+        user_id = token_payload.uid
+        user_balance = await UsersDB.get_balance(user_id=user_id, session=session)
+        return BalanceInfo(balance=user_balance)
+    elif token_payload.role == 'business':
+        business_id = token_payload.uid
+        business_balance = await BusinessDB.get_balance(business_id=business_id, session=session)
+        return BalanceInfo(balance=business_balance)
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid access token.")
