e0c6ff3fe6d2227f82294f56ec3c57abc01d2dc6
Timofey <Timofeygorev.steam@gmail.com>
2025-03-22 18:11:18 +0500

    New tests for profile (balance)
    users\business balance test

---
+++ backend/src/test/test_balance.py
@@ -0,0 +1,39 @@
+import pytest
+from httpx import AsyncClient
+from global_config import settings
+from .test_business_auth import sign_up as business_sign_up
+from .test_user_auth import sign_up as user_sign_up
+from .testers import olivia_data
+
+IP_ADDRESS = settings.IP_ADDRESS
+SERVER_PORT = settings.SERVER_PORT
+
+balance = f"http://{IP_ADDRESS}:{SERVER_PORT}/v1/api/profile/balance"
+
+@pytest.mark.asyncio
+async def test_olivia_business_balance():
+    async with AsyncClient() as client:
+        response = await client.post(url=business_sign_up, json=olivia_data)
+    assert response.status_code == 200
+    assert 'access_token' in response.json().keys()
+    assert 'refresh_token' in response.json().keys()
+    access_token = response.json()['access_token']
+
+    async with AsyncClient() as client:
+        response = await client.get(url=balance, cookies={'access_token': access_token})
+    assert response.status_code == 200
+    assert response.json()['balance'] == 0
+
+@pytest.mark.asyncio
+async def test_olivia_user_balance():
+    async with AsyncClient() as client:
+        response = await client.post(url=user_sign_up, json=olivia_data)
+    assert response.status_code == 200
+    assert 'access_token' in response.json().keys()
+    assert 'refresh_token' in response.json().keys()
+    access_token = response.json()['access_token']
+
+    async with AsyncClient() as client:
+        response = await client.get(url=balance, cookies={'access_token': access_token})
+    assert response.status_code == 200
+    assert response.json()['balance'] == 0
