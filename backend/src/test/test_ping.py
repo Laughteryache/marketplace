2b8673c3d07d219ba202d7d504317b679ed4d9d4
Timofey <Timofeygorev.steam@gmail.com>
2025-03-22 15:59:33 +0500

    test ping + update requirements.txt
    anyio==4.9.0
    pytest-asyncio==0.25.3
    pytest-tornasync==0.6.0.post2
    pytest-trio==0.8.0
    pytest-twisted==1.14.3

---
+++ backend/src/test/test_ping.py
@@ -0,0 +1,15 @@
+import pytest
+from httpx import AsyncClient
+from global_config import settings
+
+IP_ADDRESS = settings.IP_ADDRESS
+SERVER_PORT = settings.SERVER_PORT
+
+
+@pytest.mark.asyncio
+async def test_ping():
+    url = f"http://{IP_ADDRESS}:{SERVER_PORT}/ping"
+    for i in range(10):
+        async with AsyncClient() as client:
+            response = await client.get(url)
+        assert response.status_code == 200
